28fb6b56b4ff6e157684d936555fec4d8ba688ed
Muhammed Lütfü ÖĞRETİCİ <mogretici@yandex.com.tr>
2023-01-06 14:12:48 +0300

    task9

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const Post = require("./models/Post");
 
 const app = express();
 const port = 3000;
+
+//dbConnect
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://localhost/cleanblog-test-db");
 
 //template engine
 app.set("view engine", "ejs");
 
 //middlewares
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //routes
 app.get("/", (req, res) => {
-  res.render("index");
+  const posts = Post.find({}, (error, posts) => {
+    res.render("index", {
+      posts: posts,
+    });
+  });
 });
+
 app.get("/about", (req, res) => {
   res.render("about");
 });
@@ -24,6 +37,11 @@ app.get("/add_post", (req, res) => {
 app.get("/post", (req, res) => {
   res.render("post");
 });
+app.post("/sendpost", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
+});
+
 app.listen(port, () => {
   console.log(`Server started on: ${port}`);
 });
